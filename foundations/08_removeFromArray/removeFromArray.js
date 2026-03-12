7a8b0f37c5c883c56e957663ecf4328305131251
itumaini <ibrahim.tumaini254@gmail.com>
2026-03-12 17:14:07 +0300

    Added solutions to js files 06 to 10

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    return array.filter(function (element){
+        return !items.includes(element)
+    })
 };
 
 // Do not edit below this line
